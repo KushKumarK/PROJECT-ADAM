17320f9086411d5a8a911225b6ae754c7242efd7
KushKumarK <91371411+KushKumarK@users.noreply.github.com>
2021-10-31 00:03:48 +0530

    Add files via upload

---
+++ Speech_Recog/Text_to_speech.py
@@ -0,0 +1,22 @@
+
+class VoiceOver:
+    def __init__(self, voice_id, file, convertor):
+        self.convertor = convertor
+        self.voice_id = voice_id
+        self.file = file
+
+    def voice_ids(self):
+        ids = {}
+        voices = self.convertor.getProperty('voices')
+        i = 1
+
+        for voice in voices:
+            ids[i] = voice.id
+            i += 1
+        return ids
+
+    def speak(self, text):
+        self.convertor.setProperty('voice', self.voice_id)
+        self.convertor.say(text)
+        self.convertor.runAndWait()
+        return 0
